d7717fbd58695c601926faf2c7ac0e4e4aeb362f
mcaladojunior <mcaladojunior@gmail.com>
2017-01-10 20:37:19 -0200

    correção

--- src/client/client.hpp
+++ src/client/client.hpp
@@ -10,7 +10,6 @@
 #include <list>
 #include <ctime>
 #include <queue> // Fila para as classes de atraso.
-#include <array> // Array de classes de atraso.
 
 #include <boost/thread.hpp>
 #include <boost/thread/mutex.hpp>
